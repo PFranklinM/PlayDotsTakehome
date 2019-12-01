71dd866f2391d2f7ac590c525f72fb66058ee1bd
Paul Frank Mallon <paul.frank.mallon@gmail.com>
2019-11-30 23:05:46 -0800

    Dot disconnection working. Still need to differentiate between a disconnect and a square.

--- PlayDotsTakehome/Assets/Scripts/DotConnectionManager.cs
+++ PlayDotsTakehome/Assets/Scripts/DotConnectionManager.cs
@@ -15,6 +15,7 @@ public class DotConnectionManager : MonoBehaviour
     public SelectedDotColor CurrentlySelectedDotColor;
 
     private bool lineActive = false;
+
     private IndividualDot StartingDot;
     private IndividualDot PreviousDot;
     private IndividualDot TouchedDot;
@@ -27,10 +28,9 @@ public class DotConnectionManager : MonoBehaviour
     public void StartLine(Transform ClickedDot)
     {
         StartingDot = ClickedDot.GetComponent<IndividualDot>();
+        PreviousDot = StartingDot;
 
         GameBoardManager.GameBoardManagerInstance.ConnectedDots.Add(ClickedDot);
-
-        PreviousDot = ClickedDot.GetComponent<IndividualDot>();
 
         Vector3 Pos = new Vector3(ClickedDot.position.x,
             ClickedDot.position.y,
@@ -57,28 +57,33 @@ public class DotConnectionManager : MonoBehaviour
         if (lineActive)
         {
             if (Mathf.Abs(PreviousDot.Coordinates.x - dot.Coordinates.x) == 1 && Mathf.Abs(PreviousDot.Coordinates.y - dot.Coordinates.y) == 0 ||
-            Mathf.Abs(PreviousDot.Coordinates.x - dot.Coordinates.x) == 0 && Mathf.Abs(PreviousDot.Coordinates.y - dot.Coordinates.y) == 1)
+                       Mathf.Abs(PreviousDot.Coordinates.x - dot.Coordinates.x) == 0 && Mathf.Abs(PreviousDot.Coordinates.y - dot.Coordinates.y) == 1)
             {
                 if ((int)dot.ThisDotsColor == (int)PreviousDot.ThisDotsColor)
                 {
-                    if (dot == StartingDot)
-                    {
-                        Debug.Log("square made");
-                    }
-                    else
-                    {
-                        DotConnected(dot.transform);
-                    }
+                    NewDotActions(dot.transform);
                 }
             }
         }
     }
 
+    private void NewDotActions(Transform dot)
+    {
+        if (GameBoardManager.GameBoardManagerInstance.ConnectedDots.Contains(dot.transform))
+        {
+            DotDisconnected(dot);
+        }
+        else
+        {
+            DotConnected(dot);
+        }
+    }
+
     private void DotConnected(Transform NewDot)
     {
-        GameBoardManager.GameBoardManagerInstance.ConnectedDots.Add(NewDot);
-
         PreviousDot = NewDot.GetComponent<IndividualDot>();
+
+        GameBoardManager.GameBoardManagerInstance.ConnectedDots.Add(NewDot);
 
         Vector3 Pos = new Vector3(NewDot.position.x,
             NewDot.position.y,
@@ -92,6 +97,26 @@ public class DotConnectionManager : MonoBehaviour
 
         DotScoreGraphic.SetPosition(0, new Vector3(-dotScoreGraphicXPos, 0, 0));
         DotScoreGraphic.SetPosition(1, new Vector3(dotScoreGraphicXPos, 0, 0));
+    }
+
+    private void DotDisconnected(Transform NewDot)
+    {
+        PreviousDot = NewDot.GetComponent<IndividualDot>();
+
+        Vector3 Pos = new Vector3(NewDot.position.x,
+            NewDot.position.y,
+            -1);
+
+        DotConnector.SetPosition(DotConnector.positionCount - 1, Pos);
+
+        DotConnector.positionCount--;
+
+        dotScoreGraphicXPos /= 2f;
+
+        DotScoreGraphic.SetPosition(0, new Vector3(-dotScoreGraphicXPos, 0, 0));
+        DotScoreGraphic.SetPosition(1, new Vector3(dotScoreGraphicXPos, 0, 0));
+
+        GameBoardManager.GameBoardManagerInstance.ConnectedDots.RemoveAt(GameBoardManager.GameBoardManagerInstance.ConnectedDots.Count - 1);
     }
 
     private IEnumerator MouseDragLineRenderer()
